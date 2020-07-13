5c9d96562059303755591a1d6e1a68c9bf4d6ff5
MuhammadFaiez <65044999+MuhammadFaiez@users.noreply.github.com>
2020-07-14 00:41:42 +0500

    penguine and hummingbird
    complete projects

---
+++ Hummingbirds/Assets/MainMenue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenue : MonoBehaviour
+{
+public void PlayPenguin ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PlayHummingbird()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
